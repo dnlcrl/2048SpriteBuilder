2037e79e54e907cd0fb8fca5bbc37965488d6d58
danieleciriello <ciriello.daniele@gmail.com>
2014-04-18 23:11:56 +0200

    adds rounded agle to tiles

--- Source/MainScene.m
+++ Source/MainScene.m
@@ -13,6 +13,9 @@
     Grid *_grid;
     CCLabelTTF *_scoreLabel;
     CCLabelTTF *_highscoreLabel;
+    CCLabelTTF *_delta;
+    CGPoint deltaPos;
+    int oldScore;
 }
 
 - (void)dealloc {
@@ -20,7 +23,9 @@
 }
 
 - (void)didLoadFromCCB {
-    CCLOG(@"%d", _grid.score);
+    oldScore = 0;
+    [_delta runAction:[CCActionSequence actionWithArray:@[[CCActionFadeOut actionWithDuration:0.f]]]];
+    deltaPos = _delta.position;
     [_grid addObserver:self forKeyPath:@"score" options:0 context:NULL];
     [_grid addObserver:self
                                             forKeyPath:@"highscore"
@@ -37,10 +42,23 @@
 {
     if ([keyPath isEqualToString:@"score"]) {
         _scoreLabel.string = [NSString stringWithFormat:@"%d", _grid.score];
+        _delta.string = [NSString stringWithFormat:@"+ %d", _grid.score - oldScore];
+        oldScore = _grid.score;
+        CGPoint endPosition = CGPointMake(deltaPos.x,deltaPos.y - 0.04f);
+        CGPoint startPosition = deltaPos;
+        CCActionFadeIn* fadeIn = [CCActionFadeIn actionWithDuration:0.f];
+        CCActionFadeOut* fadeOut = [CCActionFadeOut actionWithDuration:0.1f];
+        CCActionMoveTo *moveTo = [CCActionMoveTo actionWithDuration:.5f position:endPosition];
+        CCActionMoveTo *reset = [CCActionMoveTo actionWithDuration:0.0f position:startPosition];
+        //CCActionRemove *hide = [CCActionHide action];
+        CCActionSequence *sequence = [CCActionSequence actionWithArray:@[fadeIn, moveTo, fadeOut, reset]];
+        [_delta runAction:sequence];
+        
     } else if ([keyPath isEqualToString:@"highscore"]) {
         _highscoreLabel.string = [NSString stringWithFormat:@"%d", _grid.highscore.intValue];
     }
 }
+
 
 
 - (void)updateHighscore {
